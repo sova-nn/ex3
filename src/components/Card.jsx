c80ccba8afb6433f55dbe0c5173bbfc468f8d547
Marina <nmo1@ya.ru>
2019-06-05 17:49:43 +0300

    Color matches is added, there are some bags, I'll try to fix in future

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -10,7 +10,7 @@ export default class Card extends React.Component{
         const cardStyle = this.props.cardStyle;
         return(
             !this.props.isClicked &&
-            <Col className='card'  style={{background: cardStyle}} onClick={(e) => this.props.onPress(this.props.cardId)} >Карточка {this.props.value}</Col>
+            <Col className='card'  style={{background: cardStyle}} onClick={(e) => this.props.onPress(this.props.cardId)} >Карточка {this.props.cardId}</Col>
         );
     }
 }
--- src/components/CardSet.jsx
+++ src/components/CardSet.jsx
@@ -7,58 +7,88 @@ export default class CardSet extends React.Component{
         super(props);
         this.state = {
             isClicked: false,
-            // color: this.cards.cardStyleColoured,
+            isPare: false,
             cards: [
-                { cardId: 0, cardStyle: 'transparent', cardStyleColoured: 'darkseagreen'},
-                { cardId: 1, cardStyle: 'transparent', cardStyleColoured: 'darkkhaki'},
-                { cardId: 2, cardStyle: 'transparent', cardStyleColoured: 'darkseagreen'},
-                { cardId: 3, cardStyle: 'transparent', cardStyleColoured: 'darkkhaki'},
-                { cardId: 4, cardStyle: 'transparent', cardStyleColoured: 'sandybrown'},
-                { cardId: 5, cardStyle: 'transparent', cardStyleColoured: 'lightseagreen'},
-                { cardId: 6, cardStyle: 'transparent', cardStyleColoured: 'lightseagreen'},
-                { cardId: 7, cardStyle: 'transparent', cardStyleColoured: 'sandybrown'},
+                { cardId: 0, cardStyle: '#61dafb', cardStyleColoured: 'darkseagreen'},
+                { cardId: 1, cardStyle: '#61dafb', cardStyleColoured: 'darkkhaki'},
+                { cardId: 2, cardStyle: '#61dafb', cardStyleColoured: 'darkseagreen'},
+                { cardId: 3, cardStyle: '#61dafb', cardStyleColoured: 'darkkhaki'},
+                { cardId: 4, cardStyle: '#61dafb', cardStyleColoured: 'sandybrown'},
+                { cardId: 5, cardStyle: '#61dafb', cardStyleColoured: 'lightseagreen'},
+                { cardId: 6, cardStyle: '#61dafb', cardStyleColoured: 'lightseagreen'},
+                { cardId: 7, cardStyle: '#61dafb', cardStyleColoured: 'sandybrown'},
             ]
         };
 
-        this.handleClick = this.handleClick.bind(this);
-        this.changeColor = this.changeColor.bind(this);
+         this.changeColor = this.changeColor.bind(this);
         this.empty = this.empty.bind(this);
+        this.matchCardsColors = this.matchCardsColors.bind(this);
+        // CardSet.isTwo = CardSet.isTwo.bind(this);
+        this.setColor = this.setColor.bind(this);
     }
 
-    // shouldComponentUpdate(nextProps, nextState) {
-    //     return this.state.cards !== nextState.cards
-    // }
 
-    handleClick(){
-        this.setState({
-            isClicked: !this.state.isClicked
-        });
-        console.log('button clicked');
-    }
 
-    // я не понимаю, почему после изменения стейта не меняются дочерние компоненты Card
+    //пока возвращение исходного цвета
     empty = () => {
         this.setState((state) => {
-            state.cards[0].cardStyle = 'transparent'
+            state.cards.map((el) => {
+                el.cardStyle = '#61dafb';
+            })
             return state;
         });
         console.log(this.state);
     }
 
+    // смена цвета при открытии карты + установка маркера
     changeColor (cardId){
         const newCards = this.state.cards;
         newCards[cardId].cardStyle = newCards[cardId].cardStyleColoured;
-        this.setState({
-            cards: newCards
-        });
+        (this.state.isClicked) ?
+            this.setState({
+                cards: newCards,
+                isClicked: true
+            })
+            :
+            this.setState({
+                cards: newCards,
+                isPare: true
+            });
         console.log(this.state);
     }
+
+
+    //проверка, что карт в объекте 2 (возвращает true/false)
+    static isTwo(cards) {
+        return (Object.keys(cards).length === 2);
+    }
+
+    //установка фона для пары одинаковых карт (их всего 2, нет смысла использовать map)
+    setColor(cardId, color) {
+        const newCards = this.state.cards;
+        newCards[cardId].cardStyle = color;
+        this.setState((state) => {
+            state.cards = newCards;
+            state.isPare = false;
+            return state;
+        });
+    }
+
+    //если цвет карт совпадает, карты будут прозрачными, если нет, то исходного цвета
+    matchCardsColors() {
+        // массив возвращает объект, содержащий 2 открытые карты
+        const openedCards = (this.state.cards.filter((card) => {return (card.cardStyle !== '#61dafb');}));
+        (CardSet.isTwo(openedCards) ? this.setColor(openedCards,'transparent') : this.setColor(openedCards,'#61dafb'));
+    }
+
+
 
 
 
 
     render() {
         const mycards = this.state.cards;
+        // this.state.isPare && this.matchCardsColors();
         return (
             <div>
                 <div className='cardset'>
@@ -72,7 +102,9 @@ export default class CardSet extends React.Component{
                                     isClicked={this.state.isClicked}
                                 />
                             )}
+
                             <button onClick={this.empty}>Мне надоело играть</button>
+                            <button onClick={this.setColor(this.state.cards, 'blue')}>Всем синий!</button>
                         </Row>
                     </Container>
                 </div>
